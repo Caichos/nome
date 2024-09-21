ec141e223b699120350bc83a63e2a1eaecf0bcfe
Caichos <00001104603342SP@al.educacao.sp.gov.br>
2024-09-20 21:04:28 -0300

    sketch.js

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(700, 500);
+  background("#E91D0E");
+  
+}
+
+function draw() {
+  stroke('blue');
+  fill('#9C27B0');
+  
+  
+  if(mouseIsPressed); {
+  rect(mouseX,mouseY,40,40);                                          }
+}
